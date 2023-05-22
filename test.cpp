e68ae750111d89df9945dc632fd71c43d9550055
Willl2510 <132473608+Willl2510@users.noreply.github.com>
2023-05-22 22:05:04 +0100

    Add files via upload
    First File

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    std::cout << "Enter Username: ";
+    string username;
+    std::cin >> username;
+    std::cout << "Username is " << username;
+}
